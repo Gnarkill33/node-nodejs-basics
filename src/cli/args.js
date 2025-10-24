647ad9a80ff35e92ead819e9d66226ef1052937e
Gnarkill33 <gnarkill33@gmail.com>
2025-10-24 22:21:06 +0300

    feat: complete args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,20 @@
 const parseArgs = () => {
-  // Write your code here
+ const argsToShow = [];
+ const [, , ...arrOfArgs] = process.argv;
+
+ let i = 0;
+
+ while (i < arrOfArgs.length) {
+  const currentArg = arrOfArgs[i];
+  if (currentArg.startsWith("--")) {
+   argsToShow.push(`${currentArg.replace("--", "")} is ${arrOfArgs[i + 1]}`);
+   i += 2;
+  } else {
+   i += 1;
+  }
+ }
+
+ console.log(argsToShow.join(", "));
 };
 
 parseArgs();
