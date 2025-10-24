e77ae3a1cb7939785c3a980250c2071f1f0575d5
Gnarkill33 <gnarkill33@gmail.com>
2025-10-24 21:52:42 +0300

    feat: complete env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,15 @@
+const PREFIX = "RSS_";
+
 const parseEnv = () => {
-  // Write your code here
+ const envVarsToShow = [];
+
+ for (let [envKey, envValue] of Object.entries(process.env)) {
+  if (envKey.startsWith(PREFIX)) {
+   envVarsToShow.push(`${envKey}=${envValue}`);
+  }
+ }
+
+ console.log(envVarsToShow.join("; "));
 };
 
 parseEnv();
