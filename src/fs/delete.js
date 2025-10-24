64210c03ee9b3529a3a38cfb8e64b7027bff0f2f
Gnarkill33 <gnarkill33@gmail.com>
2025-10-24 20:01:45 +0300

    feat: complete delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,21 @@
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+import { access, unlink } from "fs/promises";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const __filename = join(__dirname, "files", "fileToRemove.txt");
+
 const remove = async () => {
-  // Write your code here
+ try {
+  await access(__filename);
+
+  await unlink(__filename);
+ } catch (err) {
+  if (err.code === "ENOENT") {
+   throw new Error("FS operation failed");
+  }
+  throw err;
+ }
 };
 
 await remove();
