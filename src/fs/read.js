2167fd4aa20eb214a8a63284c946a8beeec36f3c
Gnarkill33 <gnarkill33@gmail.com>
2025-10-24 20:45:44 +0300

    feat: complete read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,21 @@
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+import { access, readFile } from "fs/promises";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const __filename = join(__dirname, "files", "fileToRead.txt");
+
 const read = async () => {
-  // Write your code here
+ try {
+  await access(__filename);
+
+  console.log(await readFile(__filename, { encoding: "utf8" }));
+ } catch (err) {
+  if (err.code === "ENOENT") {
+   throw new Error("FS operation failed");
+  }
+  throw err;
+ }
 };
 
 await read();
