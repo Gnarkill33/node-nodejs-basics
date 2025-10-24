030b2a0a9fe2380e8f59a1402cc9f52770b57ff0
Gnarkill33 <gnarkill33@gmail.com>
2025-10-24 16:49:05 +0300

    feat: complete create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+import { writeFile } from "fs/promises";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const __filename = join(__dirname, "files", "fresh.txt");
+
 const create = async () => {
-  // Write your code here
+ try {
+  await writeFile(__filename, "I am fresh and young", { flag: "wx" });
+ } catch (err) {
+  if (err.code === "EEXIST") {
+   throw new Error("FS operation failed");
+  }
+  throw err;
+ }
 };
 
 await create();
