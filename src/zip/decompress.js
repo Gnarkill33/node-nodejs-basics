0757bc84e03b78f618369b673c9eca8c4fb2a5f8
Gnarkill33 <gnarkill33@gmail.com>
2025-10-26 22:50:03 +0300

    feat: complete zip

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,22 @@
+import { fileURLToPath } from "url";
+import { dirname, join } from "node:path";
+import { createReadStream, createWriteStream } from "node:fs";
+import { createGunzip } from "node:zlib";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const __filename = join(__dirname, "files", "archive.gz");
+const __destDir = join(__dirname, "files", "fileToCompress.txt");
+
 const decompress = async () => {
-  // Write your code here
+ const gunzip = createGunzip();
+ const source = createReadStream(__filename);
+ const destination = createWriteStream(__destDir);
+
+ try {
+  source.pipe(gunzip).pipe(destination);
+ } catch (err) {
+  console.error("An error occurred:", err);
+ }
 };
 
 await decompress();
